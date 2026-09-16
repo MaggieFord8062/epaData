9da5e5ee6cd6dc61af1b63ac5fa5361e97adc7c3
Danny <dannorios@outlook.com>
2026-09-16 18:56:50 -0500

    Fix search.py: correct table/column names, connect to epaData.db, fix LIMIT duplication

--- search.py
+++ search.py
@@ -46,7 +46,7 @@ def search_data (
     pm_control=None
 ):
 
-    connection = sqlite3.connect("sqData.db")
+    connection = sqlite3.connect("epaData.db")
     cursor = connection.cursor()
 
     query = """
@@ -54,8 +54,8 @@ def search_data (
         FROM facility 
         JOIN unit 
             ON facility.epa_facility_id = unit.epa_facility_id
-        JOIN annual_record 
-            ON unit.internal_unit_key = annual_record.internal_unit_key
+        JOIN annual_records 
+            ON unit.internal_unit_key = annual_records.internal_unit_key
         WHERE 1=1
     """
     parameters = []
@@ -78,22 +78,22 @@ def search_data (
 
     if county:
         query += " AND facility.county = ?"
-        parameters.append(county)    
+        parameters.append(county)
 
     if latitude:
         query += " AND facility.latitude = ?"
-        parameters.append(latitude)  
+        parameters.append(latitude)
 
     if longitude:
         query += " AND facility.longitude = ?"
-        parameters.append(longitude)  
+        parameters.append(longitude)
 
     if source_category:
         query += " AND facility.source_category = ?"
-        parameters.append(source_category)      
+        parameters.append(source_category)
 
     if reporting_year:
-        query += " AND annual_record.reporting_year = ?"
+        query += " AND annual_records.year = ?"
         parameters.append(reporting_year)
 
     if primary_fuel:
@@ -106,137 +106,137 @@ def search_data (
 
     if unit_type:
         query += " AND unit.unit_type = ?"
-        parameters.append(unit_type)    
+        parameters.append(unit_type)
 
     if operating_time_operator == "Equals":
-        query += " AND annual_record.operating_time = ?"
+        query += " AND annual_records.operating_time = ?"
         parameters.append(operating_time_equals)
 
     elif operating_time_operator == "Less than":
-        query += " AND annual_record.operating_time < ?"
-        parameters.append(operating_time_max)    
+        query += " AND annual_records.operating_time < ?"
+        parameters.append(operating_time_max)
 
     elif operating_time_operator == "Greater than":
-        query += " AND annual_record.operating_time > ?"
-        parameters.append(operating_time_min)  
+        query += " AND annual_records.operating_time > ?"
+        parameters.append(operating_time_min)
 
     elif operating_time_operator == "Between":
-        query += " AND annual_record.operating_time BETWEEN ? AND ?"
-        parameters.append(operating_time_max)   
-        parameters.append(operating_time_min) 
+        query += " AND annual_records.operating_time BETWEEN ? AND ?"
+        parameters.append(operating_time_max)
+        parameters.append(operating_time_min)
 
     if gross_load_operator == "Equals":
-        query += " AND annual_record.gross_load = ?"
+        query += " AND annual_records.gross_load = ?"
         parameters.append(gross_load_equals)
 
     elif gross_load_operator == "Less than":
-        query += " AND annual_record.gross_load < ?"
-        parameters.append(gross_load_max)    
+        query += " AND annual_records.gross_load < ?"
+        parameters.append(gross_load_max)
 
     elif gross_load_operator == "Greater than":
-        query += " AND annual_record.gross_load > ?"
-        parameters.append(gross_load_min)  
+        query += " AND annual_records.gross_load > ?"
+        parameters.append(gross_load_min)
 
     elif gross_load_operator == "Between":
-        query += " AND annual_record.gross_load BETWEEN ? AND ?"
-        parameters.append(gross_load_max)   
-        parameters.append(gross_load_min)       
+        query += " AND annual_records.gross_load BETWEEN ? AND ?"
+        parameters.append(gross_load_max)
+        parameters.append(gross_load_min)
 
     if steam_load_operator == "Equals":
-        query += " AND annual_record.steam_load = ?"
+        query += " AND annual_records.steam_load = ?"
         parameters.append(steam_load_equals)
 
     elif steam_load_operator == "Less than":
-        query += " AND annual_record.steam_load < ?"
-        parameters.append(steam_load_max)    
+        query += " AND annual_records.steam_load < ?"
+        parameters.append(steam_load_max)
 
     elif steam_load_operator == "Greater than":
-        query += " AND annual_record.steam_load > ?"
-        parameters.append(steam_load_min)  
+        query += " AND annual_records.steam_load > ?"
+        parameters.append(steam_load_min)
 
     elif steam_load_operator == "Between":
-        query += " AND annual_record.steam_load BETWEEN ? AND ?"
-        parameters.append(steam_load_max)   
-        parameters.append(steam_load_min) 
+        query += " AND annual_records.steam_load BETWEEN ? AND ?"
+        parameters.append(steam_load_max)
+        parameters.append(steam_load_min)
 
     if heat_input_operator == "Equals":
-        query += " AND annual_record.heat_input = ?"
+        query += " AND annual_records.heat_input = ?"
         parameters.append(heat_input_equals)
 
     elif heat_input_operator == "Less than":
-        query += " AND annual_record.heat_input < ?"
-        parameters.append(heat_input_max)    
+        query += " AND annual_records.heat_input < ?"
+        parameters.append(heat_input_max)
 
     elif heat_input_operator == "Greater than":
-        query += " AND annual_record.heat_input > ?"
-        parameters.append(heat_input_min)  
+        query += " AND annual_records.heat_input > ?"
+        parameters.append(heat_input_min)
 
     elif heat_input_operator == "Between":
-        query += " AND annual_record.heat_input BETWEEN ? AND ?"
-        parameters.append(heat_input_max)   
-        parameters.append(heat_input_min) 
+        query += " AND annual_records.heat_input BETWEEN ? AND ?"
+        parameters.append(heat_input_max)
+        parameters.append(heat_input_min)
 
     if co2_mass_operator == "Equals":
-        query += " AND annual_record.co2_mass = ?"
+        query += " AND annual_records.co2_mass = ?"
         parameters.append(co2_mass_equals)
 
     elif co2_mass_operator == "Less than":
-        query += " AND annual_record.co2_mass < ?"
-        parameters.append(co2_mass_max)    
+        query += " AND annual_records.co2_mass < ?"
+        parameters.append(co2_mass_max)
 
     elif co2_mass_operator == "Greater than":
-        query += " AND annual_record.co2_mass > ?"
-        parameters.append(co2_mass_min)  
+        query += " AND annual_records.co2_mass > ?"
+        parameters.append(co2_mass_min)
 
     elif co2_mass_operator == "Between":
-        query += " AND annual_record.co2_mass BETWEEN ? AND ?"
-        parameters.append(co2_mass_max)   
-        parameters.append(co2_mass_min) 
+        query += " AND annual_records.co2_mass BETWEEN ? AND ?"
+        parameters.append(co2_mass_max)
+        parameters.append(co2_mass_min)
 
     if so2_mass_operator == "Equals":
-        query += " AND annual_record.so2_mass = ?"
+        query += " AND annual_records.so2_mass = ?"
         parameters.append(so2_mass_equals)
 
     elif so2_mass_operator == "Less than":
-        query += " AND annual_record.so2_mass < ?"
-        parameters.append(so2_mass_max)    
+        query += " AND annual_records.so2_mass < ?"
+        parameters.append(so2_mass_max)
 
     elif so2_mass_operator == "Greater than":
-        query += " AND annual_record.so2_mass > ?"
-        parameters.append(so2_mass_min)  
+        query += " AND annual_records.so2_mass > ?"
+        parameters.append(so2_mass_min)
 
     elif so2_mass_operator == "Between":
-        query += " AND annual_record.so2_mass BETWEEN ? AND ?"
-        parameters.append(so2_mass_max)   
-        parameters.append(so2_mass_min)    
+        query += " AND annual_records.so2_mass BETWEEN ? AND ?"
+        parameters.append(so2_mass_max)
+        parameters.append(so2_mass_min)
 
     if nox_mass_operator == "Equals":
-        query += " AND annual_record.nox_mass = ?"
+        query += " AND annual_records.nox_mass = ?"
         parameters.append(nox_mass_equals)
 
     elif nox_mass_operator == "Less than":
-        query += " AND annual_record.nox_mass < ?"
-        parameters.append(nox_mass_max)    
+        query += " AND annual_records.nox_mass < ?"
+        parameters.append(nox_mass_max)
 
     elif nox_mass_operator == "Greater than":
-        query += " AND annual_record.nox_mass > ?"
-        parameters.append(nox_mass_min)  
+        query += " AND annual_records.nox_mass > ?"
+        parameters.append(nox_mass_min)
 
     elif nox_mass_operator == "Between":
-        query += " AND annual_record.nox_mass BETWEEN ? AND ?"
-        parameters.append(nox_mass_max)   
-        parameters.append(nox_mass_min)     
+        query += " AND annual_records.nox_mass BETWEEN ? AND ?"
+        parameters.append(nox_mass_max)
+        parameters.append(nox_mass_min)
 
     if so2_control:
-        query += " AND annual_record.so2_control = ?"
+        query += " AND annual_records.so2_control_info = ?"
         parameters.append(so2_control)
 
     if nox_control:
-        query += " AND annual_record.nox_control = ?"
-        parameters.append(nox_control)    
+        query += " AND annual_records.nox_control_info = ?"
+        parameters.append(nox_control)
 
     if pm_control:
-        query += " AND annual_record.pm_control = ?"
+        query += " AND annual_records.pm_control_info = ?"
         parameters.append(pm_control)
 
 
@@ -253,38 +253,32 @@ def advance_search (
     order="DESC",
     field=None
 ):
-    connection = sqlite3.connect("sqData.db")
+    connection = sqlite3.connect("epaData.db")
     cursor = connection.cursor()
 
     if type == "Facility":
-        query = """
+        query = f"""
             SELECT *
             FROM facility
             ORDER BY {field} {order}
-            LIMIT ?
         """
 
     elif type == "Unit":
-        query = """
+        query = f"""
             SELECT *
             FROM unit
             ORDER BY {field} {order}
-            LIMIT ?
         """
 
     elif type == "Annual Record":
-        query = """
+        query = f"""
             SELECT *
             FROM annual_records
             ORDER BY {field} {order}
-            LIMIT ?
         """
 
     else:
         return []
-
-    if order == "ASC":
-        query = query.replace("DESC", "ASC")
 
     if limit:
         query += " LIMIT ?"
